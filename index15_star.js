2754eae7fc97d8624b3543e92ab2ce316edeffba
AlenaBukh <alena89xxx@gmail.com>
2024-05-04 22:47:09 +0300

    initial commit

---
+++ index15_star.js
@@ -0,0 +1,21 @@
+let numbers = [];
+
+for (let i = -10; i <= 10; i++) {
+  numbers.push(i);
+}
+//console.log(numbers);
+
+for (let i = numbers.length - 1; i >= 0; i--) {
+  if (numbers[i] < 0) {
+    numbers.splice(i, 1);
+  }
+}
+//console.log(numbers);
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i] = numbers[i] * numbers[i];
+}
+//console.log(numbers);
+
+numbers.sort((a, b) => b - a);
+console.log(numbers);
